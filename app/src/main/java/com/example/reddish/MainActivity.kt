6a532df7a9253020665d5b0ac68e4b1dbc20b76f
umangp023 <47998115+umangp023@users.noreply.github.com>
2021-03-01 20:45:01 -0500

    update

--- app/src/main/java/com/example/reddish/MainActivity.kt
+++ app/src/main/java/com/example/reddish/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.reddish
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnWelcom = findViewById<Button>(R.id.btnWelcome)
+
+        btnWelcom.setOnClickListener{
+                val intent = Intent(this, MainActivity2::class.java )
+
+                startActivity(intent)
+        }
     }
 }
